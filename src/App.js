6016314e8de52d42b0b9b8007b7e78fc6757ef04
vishal00923 <vishalbg.mz@gmail.com>
2021-11-02 03:43:15 +0530

    App Component : Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [inputText, setInputText] = useState('');
+    const [todos, setTodos] = useState([]);
+    return (
+        <div className="app">
+            <header>
+                <h1>Todo List</h1>
+            </header>
+            <Form inputText={inputText} setInputText={setInputText} todos={todos} setTodos={setTodos} />
+            <TodoList />
+        </div>
+    );
+};
 
 export default App;
