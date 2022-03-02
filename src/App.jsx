f9dbe8c3ff06d596be0fe457d7548a14c9bcf922
vishal00923 <vishalbg.mz@gmail.com>
2022-03-02 20:00:32 +0530

    update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import TodoList from './Components/todo-list/todo-list.component';
 
 import './App.scss';
 
 const App = () => {
+    const [todo, setTodo] = useState('');
+    const [todos, setTodos] = useState([]);
+    const [editTodo, setEditTodo] = useState(null);
+
     return (
         <div>
-            <TodoList />
+            <TodoList todo={todo} setTodo={setTodo} todos={todos} setTodos={setTodos} editTodo={editTodo} setEditTodo={setEditTodo} />
         </div>
     );
 };
