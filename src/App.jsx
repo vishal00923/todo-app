37a043410c5f7eb785a149c74d42726e0daf708c
vishal00923 <vishalbg.mz@gmail.com>
2022-03-02 20:21:05 +0530

    update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import TodoList from './Components/todo-list/todo-list.component';
 
 import './App.scss';
 
 const App = () => {
-    const [todo, setTodo] = useState('');
-    const [todos, setTodos] = useState([]);
-    const [editTodo, setEditTodo] = useState(null);
-
     return (
         <div>
-            <TodoList todo={todo} setTodo={setTodo} todos={todos} setTodos={setTodos} editTodo={editTodo} setEditTodo={setEditTodo} />
+            <TodoList />
         </div>
     );
 };
--- src/Components/todo-list/todo-list.component.jsx
+++ src/Components/todo-list/todo-list.component.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import TodoForm from '../todo-form/todo-form.component';
 import Todos from '../todos/todos.component';
 
 import './todo-list.styles.scss';
 
-const TodoList = ({ todo, setTodo, todos, setTodos, editTodo, setEditTodo }) => {
+const TodoList = () => {
+    const initialState = JSON.parse(localStorage.getItem('todos')) || [];
+    const [todo, setTodo] = useState('');
+    const [todos, setTodos] = useState(initialState);
+    const [editTodo, setEditTodo] = useState(null);
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos]);
+
     return (
         <div className="container">
             <h1 className="title">Todo List</h1>
