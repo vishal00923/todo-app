6203ff3a2c14b32332208ae0bf8e8b69944824c0
vishal00923 <vishalbg.mz@gmail.com>
2022-03-02 15:45:34 +0530

    add App

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import TodoList from './Components/todo-list/todo-list.component';
+
+import './App.scss';
+
+const App = () => {
+    return (
+        <div>
+            <TodoList />
+        </div>
+    );
+};
+
+export default App;
