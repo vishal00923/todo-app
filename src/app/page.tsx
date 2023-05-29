b85e1a1e042d54b817b3c27621cb9f991e951f4a
Vishal Chaurasia <vishalbg.mz@gmail.com>
2023-05-29 17:27:09 +0530

    Initial project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className='flex items-center justify-center'>
+      <h1 className='text-3xl'>Hello, world!</h1>
+    </div>
+  );
+};
+
+export default Home;
